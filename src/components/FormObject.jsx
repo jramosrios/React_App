30a2c8bbf44e912b86c38d365cb34a81f0696e06
Jesus Ramos <jramosrios@La-MacA.local>
2024-09-14 13:55:48 -0400

    Added some error messages using state and displayed album using a map filter

--- src/components/FormObject.jsx
+++ src/components/FormObject.jsx
@@ -7,6 +7,8 @@ const FormObject = (props) => {
         releaseYear: 1930,
         genre: ''
     })
+    const [error, setError] = useState('')
+    const [albumList, setAlbumList] = useState([])
 
     const handleAlbum = (e) => {
         // console.log('Event Target Name', e.target.name)
@@ -15,32 +17,64 @@ const FormObject = (props) => {
     }
 
     const createAlbum = (e) => {
+        e.preventDefault();
+        if (album.albumName.length < 3 || album.artist.length < 5 || album.releaseYear < 1930 || album.genre.length < 3) {
+            setError('Fill in the form correctly')
+        }
+        else {
+            setError('')
+            // console.log('Album', album)
 
-        e.preventDefault()
+            setAlbumList([...albumList, album])
 
-        console.log('Album', album)
-        setAlbum({
-            albumName: '',
-            artist: '',
-            releaseYear: 1930,
-            genre: ''
-        })
-
+            setAlbum({
+                albumName: '',
+                artist: '',
+                releaseYear: 1930,
+                genre: ''
+            })
+        }
     }
     return (
         <div>
             <form onSubmit={createAlbum} >
+                {
+                    error ?
+                        <p>{error}</p> :
+                        null
+                }
                 <label htmlFor="albumName">Album Name: </label>
                 <input type="text" value={album.albumName} onChange={handleAlbum} name='albumName' />
+                {
+                    album.albumName.length < 3 && album.albumName.length > 0 ?
+                        <p>Album must be at least 3 characters</p> :
+                        null
+                }
 
                 <label htmlFor="releaseYear">Release Year: </label>
                 <input type="number" value={album.releaseYear} onChange={handleAlbum} name='releaseYear' />
+                {
+                    album.releaseYear < 1930 ?
+                        <p>Release year must be over 1930</p> :
+                        null
+                }
 
                 <label htmlFor="artist">Artist : </label>
                 <input type="text" value={album.artist} onChange={handleAlbum} name='artist' />
+                {
+                    album.artist.length < 5 ?
+                        <p>Artist must be at least 5 characters long</p> :
+                        null
+                }
+
 
                 <label htmlFor="genre">Genre: </label>
                 <input type="text" value={album.genre} onChange={handleAlbum} name='genre' />
+                {
+                    album.genre.length < 3 ?
+                        <p>Genre must be at least 3 characters</p> :
+                        null
+                }
 
                 <button>Add album!</button>
             </form>
@@ -51,6 +85,19 @@ const FormObject = (props) => {
                 <p>Artist: {album.artist}</p>
                 <p>Genre: {album.genre}</p>
             </div>
+
+            {
+                albumList && albumList.length > 0 ?
+                    albumList.map((a) => (
+                        <div>
+                            <p>{a.albumName}</p>
+                            <p>{a.artist}</p>
+                            <p>{a.genre}</p>
+                            <p>{a.releaseYear}</p>
+                        </div>
+                    )) :
+                    null
+            }
 
         </div>
     )
